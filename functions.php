bde02067546bfa80e2fba1eb139c441cf475080e
Melissa Hay <melissa98@live.ca>
2022-03-03 12:42:12 -0500

    cleaned up minor details

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@ function pdo_connect_mysql() {
     // Update the details below with your MySQL details
     $DATABASE_HOST = 'localhost';
     $DATABASE_USER = 'root';
-    $DATABASE_PASS = 'Melisa98';
+    $DATABASE_PASS = '';
     $DATABASE_NAME = 'shoppingcart';
     try {
     	return new PDO('mysql:host=' . $DATABASE_HOST . ';dbname=' . $DATABASE_NAME . ';charset=utf8', $DATABASE_USER, $DATABASE_PASS);
